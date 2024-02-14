2554acdc6bee5d54ece537e43bfc30f5e8345614
julienh2000 <julien.hugues@outlook.com>
2024-02-14 21:27:49 +0100

    start shaping code for user defined oscillator

--- src/audiolib.rs
+++ src/audiolib.rs
@@ -4,27 +4,86 @@ use cpal::{
     FromSample, SizedSample,
 };
 use core::f32::consts::PI;
+use std::sync::{Arc, Mutex};
 
-pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, freq: f32, dur: u64) -> Result<(), &'static str>
+
+pub enum Waveform {
+    Sine,
+    Square,
+    Saw,
+    Triangle,
+}
+
+pub struct Oscillator {
+    pub sample_rate: f32,
+    pub waveform: Waveform,
+    pub current_sample_index: f32,
+    pub frequency_hz: f32,
+}
+
+impl Oscillator {
+    pub fn new_sine (device: &cpal::Device, config: &cpal::StreamConfig, freq: f32) -> Oscillator {
+        return Oscillator {
+            sample_rate: config.sample_rate.0 as f32,
+            waveform: Waveform::Sine,
+            current_sample_index: 0f32,
+            frequency_hz: freq,
+        }
+    }
+
+    fn next_sample_index (&mut self) {
+        self.current_sample_index = (self.current_sample_index + 1.0) % self.sample_rate;
+    }
+
+    fn calculate_sine_output_from_freq(&self, freq: f32) -> f32 {
+        (self.current_sample_index * self.frequency_hz * 2.0 * PI / self.sample_rate).sin()
+    }
+
+    fn sine_wave(&mut self) -> f32 {
+        self.next_sample_index();
+        self.calculate_sine_output_from_freq(self.frequency_hz)
+    }
+
+    fn tick(&mut self) -> f32 {
+        /*
+        match self.waveform {
+            Waveform::Sine => self.sine_wave(),
+            Waveform::Square => self.square_wave(),
+            Waveform::Saw => self.saw_wave(),
+            Waveform::Triangle => self.triangle_wave(),
+        }
+        */
+        self.sine_wave()
+    }
+}
+/*
+fn sine (osc: &mut Oscillator, next_value: &mut dyn FnMut() -> f32) {
+    let mut clock = osc.current_sample_index;
+    let mut next_value = move || {
+        clock = (clock + 1.0) % osc.sample_rate;
+        (clock * osc.frequency_hz * 2.0 * PI / osc.sample_rate).sin()
+    };
+} */
+
+pub fn run<'a, T>(device: &cpal::Device, config: &cpal::StreamConfig, dur: u64) -> Result<(), &'static str>
 where
     T: SizedSample + FromSample<f32>,
 {
-    let sample_rate = config.sample_rate.0 as f32;
+    let mut osc = Oscillator::new_sine(&device, &config, 440.0);
+
+    let _sample_rate = config.sample_rate.0 as f32;
     let channels = config.channels as usize;
 
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * freq * 2.0 * PI / sample_rate).sin()
-    };
+    let time_at_start = std::time::Instant::now();
+    println!("Time at start: {:?}", time_at_start);
 
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
+    //let osc_mutex = Arc::new(Mutex::new(osc));
     let stream = device.build_output_stream(
         config,
         move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
+            write_data(data, channels, &mut osc)
         },
         err_fn,
         None,
@@ -37,12 +96,12 @@ where
 }
 
 
-pub fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+pub fn write_data<T>(output: &mut [T], channels: usize, osc: &mut Oscillator)
 where
     T: Sample + FromSample<f32>,
 {
     for frame in output.chunks_mut(channels) {
-        let value: T = T::from_sample(next_sample());
+        let value: T = T::from_sample(osc.tick());
         for sample in frame.iter_mut() {
             *sample = value;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use cpal::traits::{DeviceTrait, HostTrait};
+//use cpal::traits::{DeviceTrait, HostTrait};
 
 mod audiolib;
-use audiolib::run;
+use audiolib::*;
+use cpal::traits::{DeviceTrait, HostTrait};
+
 
 fn main() {
+
     let host = cpal::default_host();
-    let device = host.default_output_device().expect("no output device available");
-    let config = device.default_output_config().unwrap();
+    let device = host.default_output_device().expect("no output device available !!");
+    let config = device.default_output_config().unwrap().config();
+    println!("Device: {},\nUsing config: {:?}\n", device.name().expect("no name !!"), config);
 
-    println!("Device: {},\nUsing config: {:?}\n", device.name().expect("flute"), config);
 
-    let _ = run::<f32>(&device, &config.into(), 440.0, 1000);
+
+    let _ = run::<f32>(&device, &config.into(), 1000);
 
 }
 
