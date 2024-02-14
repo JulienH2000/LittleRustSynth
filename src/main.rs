57287a0cebd27e6c119e51e057b3c400ca90a1ad
julienh2000 <julien.hugues@outlook.com>
2024-02-14 18:41:29 +0100

    fixed signal gen through cpal docs

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,64 @@
 use cpal::Sample;
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use wavegen::{sine, wf};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    FromSample, SizedSample,
+};
+use core::f32::consts::PI;
+
 
 
 fn main() {
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
     let host = cpal::default_host();
     let device = host.default_output_device().expect("no output device available");
-    let supported_config = device.default_output_config().unwrap();
+    let config = device.default_output_config().unwrap();
 
-    println!("Device: {},\nUsing config: {:?}\n", device.name().expect("flute"), supported_config);
+    println!("Device: {},\nUsing config: {:?}\n", device.name().expect("flute"), config);
 
-    let config = supported_config.into();
+    let _ = run::<f32>(&device, &config.into(), 440.0, 1000);
+
+}
 
 
-    let stream = device.build_output_stream(&config, write_data, err_fn, None).unwrap();
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, freq: f32, dur: u64) -> Result<(), &'static str>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * freq * 2.0 * PI / sample_rate).sin()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    ).unwrap();
     stream.play().unwrap();
 
-    std::thread::sleep(std::time::Duration::from_millis(3000));
+    std::thread::sleep(std::time::Duration::from_millis(dur));
+
+    Ok(())
 }
 
-fn write_data(data: &mut [f32], _: &cpal::OutputCallbackInfo) {
-    /*
-    let mut counter = 0;
-    for sample in data.iter_mut() {
-        let s = if (counter / 20) % 2 == 0 { &1.0 } else { &0.0 };
-        counter = counter + 1;
-        *sample = s.to_sample();
-    }
-    //println!("{:?}", data);
-    */
-    
-    let waveform = wf!(f32, 44100., sine!(frequency: 100., amplitude: 10.));
-    let some_samples: Vec<f32> = waveform.iter().take(44100).map(|s| s.to_sample()).collect();
-    //println!("{:?}", some_samples);
-    let mut counter = 0;
-    for sample in data.iter_mut() {
-        *sample = some_samples[counter];
-        counter = counter + 1;
-    }
-    //println!("{:?}", data);
 
-
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: Sample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = T::from_sample(next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
 }
-
